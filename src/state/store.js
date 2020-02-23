f86f292c07fbe03978a1b7608e1a7c98daf77be1
dalton <duong.daltonle@gmail.com>
2020-02-24 03:45:11 +1100

    Add Redux thunk

--- src/state/store.js
+++ src/state/store.js
@@ -3,18 +3,22 @@
  */
 
 import {
-  createStore, compose, combineReducers,
+  createStore, applyMiddleware, compose, combineReducers,
 } from 'redux'
+import thunk from 'redux-thunk'
 import * as reducers from './ducks'
 
 const rootReducer = combineReducers({ ...reducers })
 
 const initialState = {}
 
+const middleware = [thunk]
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
+    applyMiddleware(...middleware),
     // eslint-disable-next-line no-underscore-dangle
     (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose,
   ),
