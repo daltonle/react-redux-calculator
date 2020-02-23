af10f57376f55c9fb1ac4f237b942b1bab659b77
dalton <duong.daltonle@gmail.com>
2020-02-24 04:20:56 +1100

    Fix actions for calc

--- src/state/ducks/calculator/actions.js
+++ src/state/ducks/calculator/actions.js
@@ -1,17 +1,29 @@
-import store from '../../store'
+export const addKey = name => (dispatch, getState) => {
+  const operators = ['+', '-', '*', '/']
+  let expression = getState().calculator.expression.substring(0)
 
-export const addKey = keyName => (dispatch, getState) => {
-  const { expression } = getState().calculator
+  if (name === 'negate') {
+    if (expression[0] === '-') {
+      expression = expression.substring(1)
+    } else expression = `- ${expression}`
+  } else if (operators.includes(name) && operators.includes(expression[expression.length - 1])) {
+    // if user presses 2 operators consecutively, swap the operator
+    expression = `${expression.substring(0, expression.length - 1)}${name}`
+  } else {
+    expression += name
+  }
+
   dispatch({
     type: 'ADD_KEY',
-    payload: expression + keyName,
+    payload: expression,
   })
 }
 
 export const equals = () => (dispatch, getState) => {
+  const expression = getState().calculator.expression.replace(/%/g, '/100')
   dispatch({
     type: 'EQUALS',
-    payload: eval(getState().calculator.expression),
+    payload: eval(expression),
   })
 }
 
